120b4cc0974f8d2ea3d63008056e7a7ee14fac28
Martin Lindahl <martin_lindahl9@hotmail.com>
2019-01-09 20:22:13 +0100

    Added second query, feetching all orders + name

--- routes/query.js
+++ routes/query.js
@@ -16,16 +16,33 @@ connection.connect((err, res) => {
 })
 
 /* GET users listing. */
+router.get('/', function(req, res, next) {
+    let text = 'Here are all the available queries:'
+    res.render('query', {text: text})
+  })
+
+/* GET users listing. */
 router.get('/products', function(req, res, next) {
   connection.query('SELECT * from Product', (err, results, fields) => {
     console.log(err);
     console.log(results);
     let text = 'These are all the products availabe:'
-    let title = 'SELECT * from Product'
+    let title = 'Query: SELECT * from Product'
     // console.log(fields);
     res.render('queryProducts', {text: text, title: title, row: results})
   })
-  
+});
+
+/* GET users listing. */
+router.get('/orders', function(req, res, next) {
+  connection.query(`Select a.SaleID, b.Name, a.SaleDate from Sale a	JOIN (select CustomerID, Name	from	Customer) b on a.CustomerID = b.CustomerID`, (err, results, fields) => {
+    console.log(err);
+    console.log(results);
+    let text = 'These are all the products availabe:'
+    let title = 'Query: Select a.SaleID, b.Name, a.SaleDate from Sale a	JOIN (select CustomerID, Name	from	Customer) b on a.CustomerID = b.CustomerID'
+    // console.log(fields);
+    res.render('queryOrders', {text: text, title: title, row: results})
+  })
 });
 
 module.exports = router;
