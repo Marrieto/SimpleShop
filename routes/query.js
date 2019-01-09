e7a8326348ba840bbe19c64e8568e4c1e7729966
Martin Lindahl <martin_lindahl9@hotmail.com>
2019-01-09 18:43:28 +0100

    Added first query of all the products

---
+++ routes/query.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+const mysql = require('mysql')
+
+
+let connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+})
+
+connection.connect((err, res) => {
+  console.log(err);
+  console.log(res);
+})
+
+/* GET users listing. */
+router.get('/products', function(req, res, next) {
+  connection.query('SELECT * from Product', (err, results, fields) => {
+    console.log(err);
+    console.log(results);
+    let text = 'These are all the products availabe:'
+    // console.log(fields);
+    res.render('queryProducts', {text: text, row: results})
+  })
+  
+});
+
+module.exports = router;
